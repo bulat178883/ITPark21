0c35cc06afec0f20ce9393aaa1c49e60c882d50f
bulat8388 <bulat7382025@mail.ru>
2021-11-07 19:29:06 +0300

    add lesson1 and lesson2

---
+++ src/main/java/SecondProgram.java
@@ -0,0 +1,26 @@
+public class SecondProgram {
+
+    int arg1, arg2;
+
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+
+        long summa = summa(3, 5);
+        System.out.println(summa);
+        long otherSumma = summa(5, 5);
+        System.out.println(otherSumma);
+
+        long resultOfMinus = minus(20, 2);
+        System.out.println(resultOfMinus);
+
+    }
+
+    public static long summa (int arg1, int arg2){
+    int result = arg1 + arg2;
+    return result;
+    }
+
+    public static long minus (int arg1, int arg2) {return arg1 - arg2; }
+}
+
+
--- src/main/java/lesson1/firstProgram.java
+++ src/main/java/lesson1/firstProgram.java
@@ -1,7 +1,8 @@
-public class FirstProgram {
+package lesson1;
+
+public class firstProgram {
 
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
-
     }
 }
