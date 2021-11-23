e12202980df5fc6f5edd0c6f7eed5e4de14599b6
bulat8388 <bulat7382025@mail.ru>
2021-11-23 14:15:27 +0300

    исправление ошибок

--- src/main/java/lesson3/lesson3.java
+++ src/main/java/lesson3/lesson3.java
@@ -4,28 +4,22 @@ public class lesson3 {
 
     public static void main(String[] args) {
 
-        double  a, b, c;
+        double a = 0;
+        double b = 80;
+        double c = 6;
         double x1, x2;
-
-        a = 90;
-        b = 80;
-        c = 6;
-
         double Discriminant = b * b - 4 * a * c;
 
-        if (a == 0) System.out.println(" Нет действительных решений уравнени");
+        if (a == 0) System.out.println(" первый коэффициет не может 0");
 
         else if (Discriminant > 0) {
             x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
             x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
             System.out.printf("x1 = %s , x2 = %s", x1, x2);
-        }
-        else if (Discriminant == 0) {
-            x1 = (-b)/(2 * a);
+        } else if (Discriminant == 0) {
+            x1 = (-b) / (2 * a);
             System.out.printf("x1 = x2 =%s", x1);
-        }
-
-        else {
+        } else {
             System.out.println("Нет действительных решений уравнения");
 
         }
